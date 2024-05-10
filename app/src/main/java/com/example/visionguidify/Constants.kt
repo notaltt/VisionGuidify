4db01f03d348f4f5c9c3885441237fd57c73575b
Kent Alison Padigos <padigoskentalison@gmail.com>
2024-05-10 09:20:59 +0800

    new model

--- app/src/main/java/com/example/visionguidify/Constants.kt
+++ app/src/main/java/com/example/visionguidify/Constants.kt
@@ -1,6 +1,15 @@
 package com.example.visionguidify
 
 object Constants {
-    const val MODEL_PATH = "modelV5.tflite"
-    const val LABELS_PATH = "labels.txt"
+    const val MODEL_PATH = "ModelV6.tflite"
+    const val LABELS_PATH = "ModelV6.txt"
 }
+//Model v6 - trained 5/10/24
+//        labels for model v8:
+//            Chair
+//            QRCode
+//            bus
+//            car
+//            door
+//            person
+//            stair
